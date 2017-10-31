26a2a21fd2d52daefd03c3dd46c51902f20102bb
Armin Telker <armintelker@googlemail.com>
2017-10-31 17:28:11 +0100

    Sections 2

--- MKColorPicker/Classes/ColorPickerView.swift
+++ MKColorPicker/Classes/ColorPickerView.swift
@@ -218,3 +218,25 @@ open class ColorPickerView: UIView, UICollectionViewDelegate, UICollectionViewDa
     
 }
 
+
+
+class SecitonCollectionHeaderView: UICollectionViewCell {
+    let sectionLabel: UILabel
+    
+    override init(frame: CGRect) {
+        sectionLabel = UILabel(frame: CGRect(x: 15, y: 5, width: 200, height: 20))
+        sectionLabel.text = ""
+        
+        super.init(frame: frame)
+        addSubview(sectionLabel)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        sectionLabel = UILabel(frame: CGRect(x: 15, y: 5, width: 200, height: 20))
+        sectionLabel.text = ""
+        super.init(coder: aDecoder)
+        addSubview(sectionLabel)
+    }
+    
+}
+
